13de69ebecca87c2faed433f9fac323ba8cff282
pinkparrot-git <m2.raei30@gmail.com>
2025-07-17 18:49:51 +0200

    fix: remove duplicates

--- src/src/main/java/nl/gerimedica/assignment/repository/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package nl.gerimedica.assignment.repository;
-
-import nl.gerimedica.assignment.domain.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-}
--- src/src/main/java/nl/gerimedica/assignment/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package nl.gerimedica.assignment.repository;
-
-import nl.gerimedica.assignment.domain.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
